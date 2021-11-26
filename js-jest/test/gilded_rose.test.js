6ce8bc928bcc04881d88d0d779fceae7bb7b2c9e
Sergei Palmer <zigpalmer@gmail.com>
2021-11-26 10:07:34 +0000

    Added some tests

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -7,48 +7,62 @@ describe("Gilded Rose", function () {
     gildedRose.items = [];
   });
 
-  it("should store items", function () {
-    gildedRose.items.push(new Item("+5 Dexterity Vest", 10, 20));
-    expect(gildedRose.items[0].name).toBe("+5 Dexterity Vest");
-    expect(gildedRose.items[0].sellIn).toBe(10);
-    expect(gildedRose.items[0].quality).toBe(20);
+  it("should return correct result", () => {
+    // The original items passed to the method
+    const storeItems = [
+      new Item("+5 Dexterity Vest", 10, 20),
+      new Item("Aged Brie", 2, 0),
+      new Item("Elixir of the Mongoose", 5, 7),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    ];
+    // The result the code returned
+    const expectedResult = [
+      new Item("+5 Dexterity Vest", 9, 19),
+      new Item("Aged Brie", 1, 1),
+      new Item("Elixir of the Mongoose", 4, 6),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 14, 21)
+    ];
+    gildedRose.items.push(storeItems);
+    gildedRose.updateQuality();
+    const updateItems = gildedRose.items;
+    console.log(gildedRose);
+    console.log(gildedRose.items);
+
+    expect(updateItems).toStrictEqual(expectedResult);
   });
 
-  it("not allow an item to have a negative value of quality", function () {
-    gildedRose.items.push(new Item("Elixir of the Mongoose", 5, 7));
-    for (let i = 0; i < 30; i++) {
-      gildedRose.updateQuality();
-    }
+  it("not allow an item to have a negative value of quality", () => {
+    gildedRose.items.push(new Item("Elixir of the Mongoose", 5, 0));
+    gildedRose.updateQuality();
+  
     expect(gildedRose.items[0].quality).toBe(0);
   });
 
-  it("should degrade quality 2x faster when Sell by date is passed", function () {
-    gildedRose.items.push(new Item("Elixir of the Mongoose", 5, 7));
-    for (let i = 0; i < 6; i++) {
-      gildedRose.updateQuality();
-      // console.log("Day " + i);
-      // console.log("Sell In " + gildedRose.items[0].sellIn);
-      // console.log("Quality " + gildedRose.items[0].quality);
-    }
+  it("should degrade quality 2x faster when SellIn date passes", () => {
+    gildedRose.items.push(new Item("Elixir of the Mongoose", 0, 6));
+    gildedRose.updateQuality();
+    
     expect(gildedRose.items[0].sellIn).toBe(-1);
-    expect(gildedRose.items[0].quality).toBe(0);
+    expect(gildedRose.items[0].quality).toBe(4);
   });
 
-  it("should increase the quality of Aged Brie over time", function () {
+  it("should increase the quality of Aged Brie over time", () => {
     gildedRose.items.push(new Item("Aged Brie", 2, 0));
     gildedRose.updateQuality();
 
     expect(gildedRose.items[0].quality).toBe(1);
   });
 
-  it("Aged Brie increases in quality 2x after its SellIn passes", function () {
+  it("Aged Brie increases in quality 2x after its SellIn passes", () => {
     gildedRose.items.push(new Item("Aged Brie", 2, 0));
     gildedRose.updateQuality();
 
     expect(gildedRose.items[0].quality).toBe(1);
   });
 
-  it("should allow a maximum quality of 50", function () {
+  it("should allow a maximum quality of 50", () => {
     gildedRose.items.push(new Item("Aged Brie", 2, 0));
     for (let i = 0; i < 100; i++) {
       gildedRose.updateQuality();
@@ -57,7 +71,7 @@ describe("Gilded Rose", function () {
     expect(gildedRose.items[0].quality).toBe(50);
   });
 
-  it("should never decrease the quality of Sulfuras", function () {
+  it("should never decrease the quality of Sulfuras", () => {
     gildedRose.items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
     for (let i = 0; i < 100; i++) {
       gildedRose.updateQuality();
@@ -66,7 +80,7 @@ describe("Gilded Rose", function () {
     expect(gildedRose.items[0].quality).toBe(80);
   });
 
-  it("Sulfuras never has to be sold", function () {
+  it("Sulfuras never has to be sold", () => {
     gildedRose.items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
     for (let i = 0; i < 100; i++) {
       gildedRose.updateQuality();
@@ -75,7 +89,7 @@ describe("Gilded Rose", function () {
     expect(gildedRose.items[0].sellIn).toBe(0);
   });
 
-  it("Backstage passes increase in Quality by 2 when 5 < SellIn >= 10", function () {
+  it("Backstage passes increase in Quality by 2 when 5 < SellIn >= 10", () => {
     gildedRose.items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
     for (let i = 0; i < 9; i++) {
       gildedRose.updateQuality();
@@ -84,7 +98,7 @@ describe("Gilded Rose", function () {
     expect(gildedRose.items[0].quality).toBe(33);
   });
 
-  it("Backstage passes increase in Quality by 3 when SellIn <= 5", function () {
+  it("Backstage passes increase in Quality by 3 when SellIn <= 5", () => {
     gildedRose.items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
     for (let i = 0; i < 15; i++) {
       gildedRose.updateQuality();
@@ -93,19 +107,15 @@ describe("Gilded Rose", function () {
     expect(gildedRose.items[0].quality).toBe(50);
   });
 
-  it("Backstage passes quality drops to 0 when Sell In = -1", function () {
-    gildedRose.items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-    for (let i = 0; i < 16; i++) {
-      gildedRose.updateQuality();
-      // console.log("Day " + i);
-      // console.log("Sell In " + gildedRose.items[0].sellIn);
-      // console.log("Quality " + gildedRose.items[0].quality);
-    }
+  it("Backstage passes quality drops to 0 when Sell In = -1", () => {
+    gildedRose.items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20));
+    gildedRose.updateQuality();
+    
     expect(gildedRose.items[0].sellIn).toBe(-1);
     expect(gildedRose.items[0].quality).toBe(0);
   });
 
-  it("Conjured items degrade in quality 2x as normal items", function () {
+  it("Conjured items degrade in quality 2x as normal items", () => {
     gildedRose.items.push(new Item("Conjured Mana Cake", 3, 6));
     gildedRose.updateQuality();
     expect(gildedRose.items[0].sellIn).toBe(2);
